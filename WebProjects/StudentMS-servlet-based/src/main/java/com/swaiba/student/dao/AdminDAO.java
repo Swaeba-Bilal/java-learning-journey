696462fc7f2b4aa1ab316955eb05e984f351ef0a
swaiba-bilal <swaibabilal02@gmail.com>
2025-08-21 16:00:41 +0500

    Added Login Servlet

--- WebProjects/StudentMS-servlet-based/src/main/java/com/swaiba/student/dao/AdminDAO.java
+++ WebProjects/StudentMS-servlet-based/src/main/java/com/swaiba/student/dao/AdminDAO.java
@@ -1,5 +1,31 @@
 package com.swaiba.student.dao;
+import java.sql.*;
+import com.swaiba.student.db.DBConnection;
+import com.swaiba.student.model.Admin;
 
 public class AdminDAO {
-
+public Admin validateAdmin(String username,String password) {
+	Admin admin=null;
+	String sql="Select * fROM admins WHERE username=? AND password=?";
+	try (
+		Connection con=DBConnection.getConnection();
+		PreparedStatement pst= con.prepareStatement(sql)){
+		pst.setString(1,username);
+		pst.setString(1, password);
+		try 
+			(ResultSet rs=pst.executeQuery()){
+			if(rs.next()) {
+				admin=new Admin();
+				admin.setId(rs.getInt("id"));
+				admin.setUsername(rs.getString("username"));
+				admin.setPassword(rs.getString("password"));
+				
+			}
+		}
+	}
+	catch(SQLException e) {
+		e.printStackTrace();
+	}
+	return admin;
+}
 }
---
+++ WebProjects/StudentMS-servlet-based/src/main/java/com/swaiba/student/servlet/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.swaiba.student.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.swaiba.student.dao.AdminDAO;
+import com.swaiba.student.model.Admin;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private AdminDAO adminDAO;
+   public void init() {
+	   adminDAO= new AdminDAO();
+   }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		Admin admin=adminDAO.validateAdmin(username,password);
+		if(admin!=null) {
+			HttpSession session=request.getSession();
+			session.setAttribute("admin", admin);
+			response.sendRedirect("admin-dashboard.jsp");
+		}
+		else {
+			request.setAttribute("errorMessage", "Invalid username and password!");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}
+	}
+	}
