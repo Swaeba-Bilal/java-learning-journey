fbc5ad917af83801a66f83f6a78fd1b83ad1151c
swaiba-bilal <swaibabilal02@gmail.com>
2025-09-05 18:40:05 +0500

    Comments Added

--- WebProjects/PetAdoption/src/main/java/com/pet/petadoption/db/DBConnection.java
+++ WebProjects/PetAdoption/src/main/java/com/pet/petadoption/db/DBConnection.java
@@ -1,12 +1,33 @@
 package com.pet.petadoption.db;
 import java.io.IOException;
+/**
+ * Utility class for managing database connections.
+ * <p>
+ * This class loads database credential from{@Code db.properties} file
+ * located in the classpath, initializes the MYSQL JDBC driver, and provides
+ * a static method to obtain a new{@Link Connection} object whenever required.
+ * </p>
+ * <p><b>Usage Example:</b></p>
+ * <pre>
+ * try(Connection con=DBConnection.getConnection()){
+ * // Use the connection for queries
+ * }
+ * </pre>
+ * 
+ * @author Swaeba
+ * @version 1.0
+ */
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
 public class DBConnection {
+	/** Jdbc database URL(loaded from db.properties)*/
 private static String url;
+/** Database username(loaded from db.properties)*/
 private static String user;
+/** Database password(loaded from db.properties)*/
 private static String password;
+//Static initializer: loads properties and JDBC driver once
 static {
 	try(InputStream input=DBConnection.class.getClassLoader()
 			.getResourceAsStream("db.properties")){
@@ -30,7 +51,11 @@ static {
         e.printStackTrace();
     }
 }
-//Return a new connection each time
+/**Return a new connection each time
+ * 
+ * @return a new {@Link Connection} object
+ * @throws SQLException if a database access error occurs
+ */
 public static Connection getConnection() throws SQLException {
 	return DriverManager.getConnection(url,user,password);
 }
